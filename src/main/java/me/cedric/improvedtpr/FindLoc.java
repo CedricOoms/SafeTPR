82918af2b5ad257e2832d1c1da4838b891b4304e
CedricOoms <Cedric.Ooms@student.uantwerpen.be>
2023-04-24 20:33:05 +0200

    Good progress.

--- src/main/java/me/cedric/improvedtpr/FindLoc.java
+++ src/main/java/me/cedric/improvedtpr/FindLoc.java
@@ -1,24 +1,49 @@
 package me.cedric.improvedtpr;
 
-
-import com.earth2me.essentials.PlayerList;
 import com.earth2me.essentials.RandomTeleport;
+import org.bukkit.Location;
 import org.bukkit.Server;
+import org.bukkit.entity.Player;
 
+import java.lang.Math;
 
 import java.util.Collection;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 public class FindLoc {
-    private Server server;
+    private RandomTeleport tpr;
+    int minRange = 1000; //Minimum distance between old and new location
+    int maxRange = 2000; //Maximum distance between old and new location
+    int minDistance = 500; //Minimum distance between the player using tpr and other players
+    private Location NewLocFin;
+    int NumberOfPlayers;
+    Object[][] NamesAndLocations; //2D array: first row contains list of Players, second row their Locations
+    Player[] PlayerArray = NamesAndLocations[0];
+    public void getPlayersThisWorld(Server server){
+        NamesAndLocations[0][this.NumberOfPlayers] = new Player[this.NumberOfPlayers];
+        String UserName = null;
+        //Get a list of the players in this world:
+        Collection UserListCol = server.getOnlinePlayers(); //Get player names
+        Object[] UserList = UserListCol.toArray();
 
-    public FindLoc(Server server){
-        this.server=server;
-
-
-        private void getUserList {
-            Collection UserList = server.getOnlinePlayers();
-            Object[] UserListOnline = UserList.toArray();
+        //Get a list of locations for those players:
+        this.NumberOfPlayers = UserList.length;
+        for (int i = 0; i < this.NumberOfPlayers; i++){
+            UserName = UserList[i].toString(); //User's name as string, in order to use in getPlayer()
+            this.NamesAndLocations[0][i] = server.getPlayer(UserName);
+            this.NamesAndLocations[1][i] = server.getPlayer(UserName).getLocation();
         }
     }
 
+    public void getRandLoc(Location PlayerLoc){
+        Location center = tpr.getCenter();
+        CompletableFuture<Location> NewLoc = tpr.getRandomLocation(center,minRange,maxRange);
+        // Check if there are any other players near the new location:
+        for (int i = 0; i < NamesAndLocations[1].length; i++){
+            double dist = NamesAndLocations[1][i].d
+            if ()
+        }
+    }
 }
+
--- src/main/java/me/cedric/improvedtpr/doTPR.java
+++ src/main/java/me/cedric/improvedtpr/doTPR.java
@@ -1,14 +1,27 @@
 package me.cedric.improvedtpr;
 
-import com.earth2me.essentials.signs.EssentialsSign;
+import java.util.Random;
+
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
+import org.bukkit.entity.Player;
+import me.cedric.improvedtpr.FindLoc;
 
 public class doTPR implements Listener {
-    // Player right clicks an entity:
+    Random rand = new Random();
+
+    // Player right-clicks an entity:
     @EventHandler
     public void onSignTouch(PlayerInteractAtEntityEvent event){
+        Player player = event.getPlayer();
+        World world = player.getWorld();
+        Location pos = player.getLocation();
+
+
+
 
     }
 }
