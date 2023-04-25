82a4e105cf3dac08b8239221a0a7f03998f2aef6
CedricOoms <Cedric.Ooms@student.uantwerpen.be>
2023-04-25 20:35:25 +0200

    Testing.

--- src/main/java/me/cedric/improvedtpr/PlayerCreatesSignListener.java
+++ src/main/java/me/cedric/improvedtpr/PlayerCreatesSignListener.java
@@ -1,29 +1,23 @@
 package me.cedric.improvedtpr;
 
-import net.kyori.adventure.text.TextComponent;
-import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.block.Block;
-import org.bukkit.Color;
+
 import net.kyori.adventure.text.Component;
+import org.bukkit.event.block.SignChangeEvent;
 
 public class PlayerCreatesSignListener implements Listener {
     Component text= Component.text("[TPR]");
     @EventHandler
-    public void OnPlayerCreatesSign(BlockPlaceEvent event){
+    public void OnPlayerCreatesSign(SignChangeEvent event){
         Player p = event.getPlayer();
-        Block PlacedBlock = event.getBlock();
-        if (PlacedBlock instanceof Sign){
-            Sign sign = (Sign) PlacedBlock;
-            p.sendPlainMessage("It's a sign bro");
-            Component FirstLine = sign.line(0);
-            if ( text == FirstLine ){
-                //sign.line(0,Color.BLUE + "[TPR]");
-                p.sendPlainMessage("Successfully made TPR sign!");
+        p.sendPlainMessage("It worked!");
+        Component FirstLine = event.line(0);
+        if ( FirstLine.equals(text) ){
+            //sign.line(0,Color.BLUE + "[TPR]");
+            p.sendPlainMessage("Successfully made TPR sign!");
             }
-        }
+
     }
 }
